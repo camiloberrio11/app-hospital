81a1ee95c9f8d90512e4627d162e201c69b2bc9e
camiloberrio <camiloberriouribe@gmail.com>
2021-04-19 23:40:40 -0500

    component notfound 404 implemented

--- src/app/pages/notfound/notfound.component.ts
+++ src/app/pages/notfound/notfound.component.ts
@@ -3,14 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-notfound',
   templateUrl: './notfound.component.html',
-  styles: [
-  ]
+  styleUrls: ['./notfound.component.css'],
 })
 export class NotfoundComponent implements OnInit {
+  year = new Date().getFullYear();
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
